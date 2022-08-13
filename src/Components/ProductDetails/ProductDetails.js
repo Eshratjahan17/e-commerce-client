37effeef59d4ee943498f0c969f8b35c778cd87b
Eshratjahan17 <eshratjahan701@gmail.com>
2022-08-13 10:32:58 +0600

    product Ui

--- src/Components/ProductDetails/ProductDetails.js
+++ src/Components/ProductDetails/ProductDetails.js
@@ -15,7 +15,23 @@ const ProductDetails = () => {
   },[]);
   return (
     <div>
-     {product.name}
+      <div class="hero min-h-screen bg-base-200">
+        <div class="hero-content flex-col lg:flex-row">
+          <img
+            src={product.picture}
+            class="max-w-sm rounded-lg shadow-2xl"
+          />
+          <div>
+            <h1 class="text-5xl font-bold">{product.name}!</h1>
+            <p class="py-6">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
+            <button class="btn btn-primary">BUY NOW</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
